6967082a1beb82d79e016cdf6c0f5193d615e4a9
Mike Gracia <michael.gracia@sisinc.org>
2017-09-20 09:29:56 -0400

    move schedule outside of main script

---
+++ schedule.py
@@ -0,0 +1,24 @@
+
+channels = 16
+hours = 24
+schedule = [[0 for x in range(channels)] for y in range(hours)]
+
+
+#these default to 0 if not set
+for x in range(channels):
+  schedule[ 8][x] = 0 #8AM
+  schedule[ 9][x] = 0
+  schedule[10][x] = 0
+  schedule[11][x] = 0
+  schedule[12][x] = 0
+  schedule[13][x] = 10 #01PM
+  schedule[14][x] = 30
+  schedule[15][x] = 30
+  schedule[16][x] = 80
+  schedule[17][x] = 100 #05PM
+  schedule[18][x] = 100
+  schedule[19][x] = 100
+  schedule[20][x] = 80
+  schedule[21][x] = 50 #9PM
+  schedule[22][x] = 0
+  schedule[23][x] = 0
--- whet.py
+++ whet.py
@@ -12,7 +12,8 @@ from weatherType import WeatherType
 import PCA9685
 import PCA9685_dummy
 import sys
-import pygame
+import pygame   #sounds
+import schedule
 
 
 pid = str(os.getpid())
@@ -29,10 +30,15 @@ logDir = 'logs/'
 if not os.path.exists(logDir):
     os.makedirs(logDir)
 logging.basicConfig(filename= logDir + str(datetime.now()) + '.log',level=logging.INFO)
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
 
 # Initialise the PCA9685, if cant use a dummy (for testing on machine that is not pi)
 try:
   pwm = PCA9685.PCA9685()
+  # Alternatively specify a different address and/or bus:
+  #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 except:
   msg = "WARNING - unable to load PWM chip... no values will set!"
   logging.info(msg)
@@ -42,56 +48,18 @@ except:
 
 
 
-
-
-
-
-
-w, h = 16, 24
-Matrix = [[0 for x in range(w)] for y in range(h)]
-
-for x in range(w):
-  Matrix[ 8][x] = 0 #8AM
-  Matrix[ 9][x] = 0
-  Matrix[10][x] = 0
-  Matrix[11][x] = 0
-  Matrix[12][x] = 0
-  Matrix[13][x] = 10 #01PM
-  Matrix[14][x] = 30
-  Matrix[15][x] = 30
-  Matrix[16][x] = 80
-  Matrix[17][x] = 100 #05PM
-  Matrix[18][x] = 100
-  Matrix[19][x] = 100
-  Matrix[20][x] = 80
-  Matrix[21][x] = 50 #9PM
-  Matrix[22][x] = 0
-  Matrix[23][x] = 0
-
-
-
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
-
-
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
-
 # Configure min and max, initial weather pattern
 led_max = 4095  # Max Brightness
 led_min = 0     # Min Brightness (off)
 run = True
 weather = WeatherType.normal
+Matrix = schedule.schedule
 
 
 
 
 
-
-# Set frequency to 60hz for servos / 1000hz LEDS.
+# Set frequency to 1000hz... LEDS.
 pwm.set_pwm_freq(1000)
 pwm.set_all(led_min)
 time.sleep(1)
