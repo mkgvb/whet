f8d7d86b9bc92da4448df701ef231cc48b600405
Mike Gracia <michael.gracia@sisinc.org>
2017-10-11 10:30:23 -0400

    implement rotating logs and clean up main script

--- whet.py
+++ whet.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
+import logging
 import Server
 import time
+from logging.handlers import TimedRotatingFileHandler
+import os
+#Logs----------------------------------------------------------
+logDir = 'logs/'
+if not os.path.exists(logDir):
+    os.makedirs(logDir)
+handler = TimedRotatingFileHandler(logDir+"whet.log",
+                                    when='midnight',
+                                    interval=1,
+                                    backupCount=7)
+logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s',
+                    level=logging.DEBUG,
+                    handlers=[handler])
+
+logging.info("Whet started")
+
+#Server----------------------------------------------------------
 Server = Server.Server()
 Server.start()
 time.sleep(1)
 
-import logging
 import math
-import os
 import random
 import threading
 import wsclient
@@ -26,12 +42,6 @@ ls = LightSchedule.LightSchedule()
 s = Settings.Settings()
 ws = wsclient
 
-# LOGGING
-logDir = 'logs/'
-if not os.path.exists(logDir):
-    os.makedirs(logDir)
-logging.basicConfig(filename=logDir + str(datetime.now()) +
-                    '.log', level=logging.INFO)
 # Uncomment to enable debug output.
 #import logging
 # logging.basicConfig(level=logging.DEBUG)
@@ -60,21 +70,13 @@ pwm.set_pwm_freq(1000)
 pwm.set_all(LED_MIN)
 time.sleep(1)
 
-
-def timeStr(_t):
-    return _t.strftime('%H:%M:%S')
-
-
-cur = LED_MIN
-
 try:
-
     threads = []
     for i in range(ls.get_number_of_channels()):
         channelObj = Channel.Channel(i, pwm)
         threads.append(channelObj)
         channelObj.start()
-    
+
     # keep main thread alive
     while True:
         
@@ -90,22 +92,17 @@ try:
                 wave_obj = sa.WaveObject.from_wave_file("sound/c1.wav")
                 play_obj = wave_obj.play()
             except:
-                print("Cant play cloud audio")
+                logging.warn("Cant play cloud audio")
             WEATHER = WeatherType.cloudy
             cloudLength = random.randint(30, 60)
-            print(timeStr(datetime.now()) + ' : Starting clouds... Length = ' + str(cloudLength))
-            logging.info(timeStr(datetime.now()) +
-                         ' : Starting clouds... Length = ' + str(cloudLength))
+            logging.info('Starting clouds... Length = ' + str(cloudLength))
             time.sleep(cloudLength)
 
         # RANDOM STORM
         if s.weather=="storm" or (s.storms_random_on and datetime.now().hour >= s.storms_random_start_time and random.randint(1, s.storms_random_freq) == 1):
             WEATHER = WeatherType.storm
             stormLength = random.randint(60, 120)
-            print(timeStr(datetime.now()) +
-                  ' : Starting thunderstorm... Length = ' + str(stormLength))
-            logging.info(timeStr(datetime.now()) +
-                         ' : Starting thunderstorm... Length = ' + str(stormLength))
+            logging.info('Starting thunderstorm... Length = ' + str(stormLength))
             time.sleep(stormLength)
 
         s.weather = "normal"
