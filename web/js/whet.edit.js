9e0cb4c46d7e2d2cf9f2634ef4aef1df69760bb5
Mike Gracia <michael.gracia@sisinc.org>
2017-10-17 10:30:08 -0400

    removed a bunch of redundent js and fixed the log window style to be responsive

--- web/js/whet.edit.js
+++ web/js/whet.edit.js
@@ -7,54 +7,24 @@ $(function () {
     if (conn == null)
         connect();
 
-
-    function log(msg) {
-        var control = $('#log');
-        control.html(control.html() + msg + '<br/>');
-        control.scrollTop(control.scrollTop() + 200);
-    }
     function connect() {
         disconnect();
-        var transports = $('#protocols input:checked').map(function () {
-            return $(this).attr('id');
-        }).get();
+
         conn = new SockJS('http://' + window.location.host + '/chat', 0);
-        log('Connecting...');
+
         conn.onopen = function () {
             log('Connected.');
             conn.send('{"request":"light_schedule"}');
-            conn.send('{"request":"settings"}')
             update_ui();
         };
         conn.onmessage = function (e) {
 
             var eparsed = JSON.parse(e.data);
-            log('Received: ' + JSON.stringify(eparsed, null, 2));
-
-            if (eparsed.type == "user")
-                log("User did something");
-
-            if (eparsed.channel != null) {
-            }
-
 
             if (eparsed.channels != null) {
-                lightSchedule = eparsed.channels;
-                edit(lightSchedule);
-                draw_lightSchedule_graph(lightSchedule);
+                edit(eparsed.channels);
+
             }
-
-            if (eparsed.settings != null) {
-                settings = eparsed.settings;
-            }
-
-            var d = new Date();
-            var n = "(" + d.getHours() + ")" + d.toLocaleTimeString();
-            $("#time").text(n);
-
-
-
-
 
         };
         conn.onclose = function () {
@@ -63,14 +33,7 @@ $(function () {
             update_ui();
         };
     }
-    function disconnect() {
-        if (conn != null) {
-            log('Disconnecting...');
-            conn.close();
-            conn = null;
-            update_ui();
-        }
-    }
+
     function update_ui() {
         var msg = '';
         if (conn == null || conn.readyState != SockJS.OPEN) {
@@ -81,133 +44,8 @@ $(function () {
             $('#connect').text('Disconnect');
         }
     }
-    $('#connect').click(function () {
-        if (conn == null) {
-            connect();
-        } else {
-            disconnect();
-        }
-        update_ui();
-        return false;
-    });
-    $('form').submit(function () {
-        var text = $('#text').val();
-        log('Sending: ' + text);
-        conn.send(text);
-        var tosend = JSON.stringify(lightSchedule);
-        toSend = '{"update":' + tosend + "}";
-        var skip = 0;
-        conn.send(toSend);
-        $('#text').val('').focus();
-        return false;
-    });
 
-    function draw_lightSchedule_graph(channels) {
-        var ds0 = [];
-        var ds1 = [];
-        var ds2 = [];
-        var ds3 = [];
-        var labels = []
-        for (i = 0; i < 24; i++) {
-            ds0.push(channels[0].schedule[i].percent);
-            ds1.push(channels[1].schedule[i].percent);
-            ds2.push(channels[2].schedule[i].percent);
-            ds3.push(channels[3].schedule[i].percent);
-            labels.push(channels[0].schedule[i].hour);
-        }
-
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Channel 0',
-                        data: ds0,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Channel 1',
-                        data: ds1,
-                        backgroundColor: [
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Channel 2',
-                        data: ds2,
-                        backgroundColor: [
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Channel 3',
-                        data: ds3,
-                        backgroundColor: [
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }
-
-                ]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-    }
+   
 
     function edit(eChannels) {
         // Set default options
--- web/js/whet.js
+++ web/js/whet.js
@@ -1,286 +1,280 @@
-$(function () {
-    var channels = [];
-    var lightSchedule = {};
-    var settings = {};
-    var conn = null;
 
-    if (conn == null)
-        connect();
+var channels = [];
+var lightSchedule = {};
+var settings = {};
+var conn = null;
 
-
-    function log(msg) {
-        var control = $('#log');
-        control.html(control.html() + msg + '<br/>');
-        control.scrollTop(control.scrollTop() + 200);
-    }
-    function connect() {
-        disconnect();
-        var transports = $('#protocols input:checked').map(function () {
-            return $(this).attr('id');
-        }).get();
-        conn = new SockJS('http://' + window.location.host + '/chat', transports);
-        log('Connecting...');
-        conn.onopen = function () {
-            log('Connected.');
-            conn.send('{"request":"light_schedule"}');
-            conn.send('{"request":"settings"}')
-            update_ui();
-        };
-        conn.onmessage = function (e) {
-
-            var eparsed = JSON.parse(e.data);
-            log('Received: ' + JSON.stringify(eparsed, null, 2));
-
-            if (eparsed.type == "user")
-                log("User did something");
-
-            if (eparsed.channel != null) {
-                draw_pwmChannel(eparsed.channel);
+if (conn == null)
+    connect();
 
 
-            }
-
-
-
-            if (eparsed.channels != null) {
-                lightSchedule = eparsed.channels;
-                //draw_lightSchedule_Table(lightSchedule);
-                draw_lightSchedule_graph(lightSchedule);
-            }
-
-            if (eparsed.settings != null) {
-                settings = eparsed.settings;
-                draw_settings(settings);
-            }
-
-            var d = new Date();
-            var n = "(" + d.getHours() + ")" + d.toLocaleTimeString();
-            $("#time").text(d);
-
-
-
-
-
-        };
-        conn.onclose = function () {
-            log('Disconnected.');
-            conn = null;
-            update_ui();
-        };
-    }
-    function disconnect() {
-        if (conn != null) {
-            log('Disconnecting...');
-            conn.close();
-            conn = null;
-            update_ui();
-        }
-    }
-    function update_ui() {
-        var msg = '';
-        if (conn == null || conn.readyState != SockJS.OPEN) {
-            $('#status').text('disconnected');
-            $('#connect').text('Connect');
-        } else {
-            $('#status').text('connected (' + conn.protocol + ')');
-            $('#connect').text('Disconnect');
-        }
-    }
-    $('#connect').click(function () {
-        if (conn == null) {
-            connect();
-        } else {
-            disconnect();
-        }
+function log(msg) {
+    var control = $('#log');
+    control.html(control.html() + msg + '<br/>');
+    control.scrollTop(control.scrollTop() + 200);
+}
+function connect() {
+    disconnect();
+    conn = new SockJS('http://' + window.location.host + '/chat', 0);   //last param is transport, 0 is websocket
+    log('Connecting...');
+    conn.onopen = function () {
+        log('Connected.');
+        conn.send('{"request":"light_schedule"}');
+        conn.send('{"request":"settings"}')
         update_ui();
-        return false;
-    });
-    $('form').submit(function () {
-        var text = $('#text').val();
-        log('Sending: ' + text);
-        conn.send(text);
-        var tosend = JSON.stringify(lightSchedule);
-        toSend = '{"light_schedule":' + tosend + "}";
-        var skip = 0;
-        conn.send(toSend);
-        $('#text').val('').focus();
-        return false;
-    });
+    };
+    conn.onmessage = function (e) {
+
+        var eparsed = JSON.parse(e.data);
+        log('Received: ' + JSON.stringify(eparsed, null, 2));
+
+        if (eparsed.type == "user")
+            log("User did something");
+
+        if (eparsed.channel != null) {
+            draw_pwmChannel(eparsed.channel);
 
 
-    function draw_pwmChannel(c_obj) {
-        var content = $("#channel-statuses");
-
-        if (content.find("#" + c_obj.c_id).length === 0)
-        {
-            var j = content.find("#99").clone();
-            j.attr("id",c_obj.c_id);
-            j.removeClass("invisible");
-            j.appendTo(content);
         }
 
-        var row = content.find("#" + c_obj.c_id);
-        var channel = row.find(".channel-id");
-        var pwm_val = row.find('.pwm-value');
-        var progress = row.find('.progress-bar');
-        var progress_text = row.find(".progress-text");
-
-        $(channel).text("Channel" + c_obj.c_id);
-        $(pwm_val).text(c_obj.cur + "| sleep= " + c_obj.sleepTime + " | goal= " + c_obj.goal);
-        $(progress).attr("aria-valuenow", c_obj.percent);
-        $(progress).attr("style", "width:" + c_obj.percent + "%");
-        $(progress_text).text(c_obj.percent + "%");
-
-        //sort the children
-        var listitems = content.children("div");
-        listitems.sort(function(a, b) {
-            var compA = $(a).text().toUpperCase();
-            var compB = $(b).text().toUpperCase();
-             //console.log((compA < compB) ? -1 : (compA > compB) ? 1 : 0);
-            return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-         })
-         $(content).append(listitems);
 
 
+        if (eparsed.channels != null) {
+            lightSchedule = eparsed.channels;
+            //draw_lightSchedule_Table(lightSchedule);
+            draw_lightSchedule_graph(lightSchedule);
+        }
+
+        if (eparsed.settings != null) {
+            settings = eparsed.settings;
+            draw_settings(settings);
+        }
+
+        var d = new Date();
+        var n = "(" + d.getHours() + ")" + d.toLocaleTimeString();
+        $("#time").text(d);
+
+
+
+
+
+    };
+    conn.onclose = function () {
+        log('Disconnected.');
+        conn = null;
+        update_ui();
+    };
+}
+function disconnect() {
+    if (conn != null) {
+        log('Disconnecting...');
+        conn.close();
+        conn = null;
+        update_ui();
+    }
+}
+function update_ui() {
+    var msg = '';
+    if (conn == null || conn.readyState != SockJS.OPEN) {
+        $('#status').text('disconnected');
+        $('#connect').text('Connect');
+    } else {
+        $('#status').text('connected (' + conn.protocol + ')');
+        $('#connect').text('Disconnect');
+    }
+}
+$('#connect').click(function () {
+    if (conn == null) {
+        connect();
+    } else {
+        disconnect();
+    }
+    update_ui();
+    return false;
+});
+$('form').submit(function () {
+    var text = $('#text').val();
+    log('Sending: ' + text);
+    conn.send(text);
+    var tosend = JSON.stringify(lightSchedule);
+    toSend = '{"light_schedule":' + tosend + "}";
+    var skip = 0;
+    conn.send(toSend);
+    $('#text').val('').focus();
+    return false;
+});
+
+
+function draw_pwmChannel(c_obj) {
+    var content = $("#channel-statuses");
+
+    if (content.find("#" + c_obj.c_id).length === 0) {
+        var j = content.find("#99").clone();
+        j.attr("id", c_obj.c_id);
+        j.removeClass("invisible");
+        j.appendTo(content);
     }
 
-    function draw_settings(settings) {
+    var row = content.find("#" + c_obj.c_id);
+    var channel = row.find(".channel-id");
+    var pwm_val = row.find('.pwm-value');
+    var progress = row.find('.progress-bar');
+    var progress_text = row.find(".progress-text");
 
-        var content = $("#settings-content");
-        content.empty();
+    $(channel).text("Channel" + c_obj.c_id);
+    $(pwm_val).text(c_obj.cur + "| sleep= " + c_obj.sleepTime + " | goal= " + c_obj.goal);
+    $(progress).attr("aria-valuenow", c_obj.percent);
+    $(progress).attr("style", "width:" + c_obj.percent + "%");
+    $(progress_text).text(c_obj.percent + "%");
 
-        for (var property in settings) {
-            if (settings.hasOwnProperty(property)) {
-
-                var row = document.createElement('div');
-                row.className = 'form-group row';
-
-                var label = document.createElement('label');
-                label.className = 'col-3 col-form-label';
-                label.innerText = property.replace(/_/g, " ");
-                label.setAttribute('for', property);
-
-                var input = document.createElement('input');
-                input.className = 'form-control';
-
-                var input_wrap = document.createElement('div');
-                input_wrap.className = 'col-2';
-                input_wrap.appendChild(input);
-
-                if (typeof settings[property] === "number" || typeof settings[property] === "string") {
-                    input.type = "textbox";
-                    input.name = property;
-                    input.value = settings[property];
-                }
-                if (typeof settings[property] === "boolean") {
-                    input.type = "checkbox";
-                    input.name = property;
-                    input.id = property;
-                    input.checked = settings[property];
-                    $(input).bootstrapSwitch();
-
-                }
+    //sort the children
+    var listitems = content.children("div");
+    listitems.sort(function (a, b) {
+        var compA = $(a).text().toUpperCase();
+        var compB = $(b).text().toUpperCase();
+        //console.log((compA < compB) ? -1 : (compA > compB) ? 1 : 0);
+        return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+    })
+    $(content).append(listitems);
 
 
+}
 
-                row.appendChild(label);
-                row.appendChild(input_wrap);
-                content.append(row);
+function draw_settings(settings) {
 
-                // content.append("<div class='row'>"
-                // +"<div class='col col-lg-2'>"+ property + "</div>")
-                // content.append(input);
-                // content.append("</div> </div>")
+    var content = $("#settings-content");
+    content.empty();
 
-                // var 
+    for (var property in settings) {
+        if (settings.hasOwnProperty(property)) {
+
+            var row = document.createElement('div');
+            row.className = 'form-group row';
+
+            var label = document.createElement('label');
+            label.className = 'col-3 col-form-label';
+            label.innerText = property.replace(/_/g, " ");
+            label.setAttribute('for', property);
+
+            var input = document.createElement('input');
+            input.className = 'form-control';
+
+            var input_wrap = document.createElement('div');
+            input_wrap.className = 'col-2';
+            input_wrap.appendChild(input);
+
+            if (typeof settings[property] === "number" || typeof settings[property] === "string") {
+                input.type = "textbox";
+                input.name = property;
+                input.value = settings[property];
             }
-        }
-    }
+            if (typeof settings[property] === "boolean") {
+                input.type = "checkbox";
+                input.name = property;
+                input.id = property;
+                input.checked = settings[property];
+                $(input).bootstrapSwitch();
+
+            }
 
 
-    function draw_lightSchedule_Table(channels) {
 
-        var content = $("#lightSchedule-content")
-        content.empty();
-        content.append("<th>" + "#" + "</th>")
-
-        for (h = 0; h < 24; h++) {
-            content.append("<th>" + h + "</th>")
-        }
-        for (var i = 0; i < channels.length; i++) {
-            var row = document.createElement('tr');
-            $(row).append("<th>" + channels[i].id + "</th>");   //channel name
+            row.appendChild(label);
+            row.appendChild(input_wrap);
             content.append(row);
 
-            for (var j = 0; j < channels[i].schedule.length; j++) {
-                var column = document.createElement('td');
-                var input = document.createElement("input");
-                //$(input).attr("class","form-control");
-                $(input).attr("type", "number");
-                // $(input).attr("size","3");
-                $(input).attr("max","100");
-                $(input).attr("value", channels[i].schedule[j].percent);
-                column.appendChild(input);
-                //column.innerText = channels[i].schedule[j].percent;
-                row.appendChild(column);
-            }
+            // content.append("<div class='row'>"
+            // +"<div class='col col-lg-2'>"+ property + "</div>")
+            // content.append(input);
+            // content.append("</div> </div>")
 
-
+            // var 
         }
     }
+}
 
-    function hexToRgb(hex, alpha) {
-        if (hex == undefined)
-            hex = '#00000';
-        hex   = hex.replace('#', '');
-        var r = parseInt(hex.length == 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
-        var g = parseInt(hex.length == 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
-        var b = parseInt(hex.length == 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
-        if ( alpha ) {
-           return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+
+function draw_lightSchedule_Table(channels) {
+
+    var content = $("#lightSchedule-content")
+    content.empty();
+    content.append("<th>" + "#" + "</th>")
+
+    for (h = 0; h < 24; h++) {
+        content.append("<th>" + h + "</th>")
+    }
+    for (var i = 0; i < channels.length; i++) {
+        var row = document.createElement('tr');
+        $(row).append("<th>" + channels[i].id + "</th>");   //channel name
+        content.append(row);
+
+        for (var j = 0; j < channels[i].schedule.length; j++) {
+            var column = document.createElement('td');
+            var input = document.createElement("input");
+            //$(input).attr("class","form-control");
+            $(input).attr("type", "number");
+            // $(input).attr("size","3");
+            $(input).attr("max", "100");
+            $(input).attr("value", channels[i].schedule[j].percent);
+            column.appendChild(input);
+            //column.innerText = channels[i].schedule[j].percent;
+            row.appendChild(column);
         }
-        else {
-           return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-        }
-     }
 
-    function draw_lightSchedule_graph(channels) {
-        var outtie = [];
-        var labels = [];
-        
-        channels.forEach(function(item, index, array) {
-            var s = {};
-            labels = [];    //erase previous only want one copy
-            s.label = item.id;
-            s.data = [];
-            item.schedule.forEach(function(item_n, index_n, array_n) {
-                s.data.push(item_n.percent);
-                labels.push(item_n.hour)
-              });
-            s.backgroundColor = [hexToRgb(item.color, 0.2)];
-            s.borderColor = [hexToRgb(item.color, 0.2)];
-            s.borderWidth = 1;
-            outtie.push(s);
 
-          });
+    }
+}
 
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: outtie
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
+function hexToRgb(hex, alpha) {
+    if (hex == undefined)
+        hex = '#00000';
+    hex = hex.replace('#', '');
+    var r = parseInt(hex.length == 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
+    var g = parseInt(hex.length == 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
+    var b = parseInt(hex.length == 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
+    if (alpha) {
+        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+    }
+    else {
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+}
+
+function draw_lightSchedule_graph(channels) {
+    var outtie = [];
+    var labels = [];
+
+    channels.forEach(function (item, index, array) {
+        var s = {};
+        labels = [];    //erase previous only want one copy
+        s.label = item.id;
+        s.data = [];
+        item.schedule.forEach(function (item_n, index_n, array_n) {
+            s.data.push(item_n.percent);
+            labels.push(item_n.hour)
         });
-    }
+        s.backgroundColor = [hexToRgb(item.color, 0.2)];
+        s.borderColor = [hexToRgb(item.color, 0.2)];
+        s.borderWidth = 1;
+        outtie.push(s);
 
-});
+    });
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: outtie
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
